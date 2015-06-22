31cdb7f429b730276c2e79a4b5e0b469f25f6d50
tmaung <tmaung@puppetlabs.vm>
2015-06-22 17:10:05 +0000

    More variables

--- manifests/config.pp
+++ manifests/config.pp
@@ -3,11 +3,24 @@
 # This class is called from nifi for service config.
 #
 class nifi::config (
-  $global_owner   = $::nifi::params::global_owner,
-  $global_group   = $::nifi::params::global_group,
-  $banner_text    = $::nifi::params::banner_text,
+  $global_owner            = $::nifi::params::global_owner,
+  $global_group            = $::nifi::params::global_group,
 
-  $conf_template  = 'nifi/conf/nifi.properties.erb',
+  $conf_template           = 'nifi/conf/nifi.properties.erb',
+  $bootstrap_template      = 'nifi/conf/bootstrap.conf.erb',
+
+  $security_limits_file    = $::nifi::params::security_limits_file,
+  $is_ncm                  = $::nifi::params::is_ncm,
+  $cluster_mode            = $::nifi::params::cluster_mode,
+
+  $java_xms                = $::nifi::params::java_xms,
+  $java_xmx                = $::nifi::params::java_xmx,
+
+  $flow_file               = $::nifi::params::flow_file,
+  $flow_archive_dir        = $::nifi::params::flow_archive_dir,
+  $flow_autoresume         = $::nifi::params::flow_autoresume,
+  $banner_text             = $::nifi::params::banner_text,
+
 ) inherits ::nifi::params {
   
   validate_string($banner_text)
@@ -15,6 +28,7 @@ class nifi::config (
   File {
     owner => $global_owner,
     group => $global_group,
+    mode  => $global_mode,
   }
 
   file { $conf_dir:
@@ -23,7 +37,15 @@ class nifi::config (
 
   file { "${conf_dir}/nifi.properties":
     ensure  => file,
-    mode    => '0640',
     content => template($conf_template),
   } 
+  
+  file { "${conf_dir}/bootstrap.conf":
+    ensure  => file,
+    content => template($bootstrap_template),
+  } 
+  
+  unless $is_ncm {
+    ## Handle Ulimits: 
+  }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -6,12 +6,14 @@
 class nifi::params {
   case $::osfamily {
     'Debian': {
-      $package_name = 'nifi'
-      $service_name = 'nifi'
+      $package_name         = 'nifi'
+      $service_name         = 'nifi'
+      $security_limits_file = '/etc/security/limits.conf'
     }
     'RedHat', 'Amazon': {
-      $package_name = 'nifi'
-      $service_name = 'nifi'
+      $package_name         = 'nifi'
+      $service_name         = 'nifi'
+      $security_limits_file = '/etc/security/limits.conf'
     }
     default: {
       fail("${::operatingsystem} not supported")
@@ -23,5 +25,18 @@ class nifi::params {
   $global_group         = 'root'
   $global_mode          = '0640'
 
+  $conf_dir             = '/opt/nifi/default/conf'
+  $lib_dir              = '/opt/nifi/default/lib'
+  $is_ncm               = false
+  $cluster_mode         = false
+
+  ## Bootstrap Configurations
+  $java_xms             = '256MB'
+  $java_xmx             = '512MB'
+
+  ## Core Configuration settings
   $banner_text          = 'TODO: OP NIFI MODULE'
+  $flow_file            = './conf/flow.xml.gz'
+  $flow_archive_dir     = './conf/archive/'
+  $flow_autoresume      = 'true'
 }
